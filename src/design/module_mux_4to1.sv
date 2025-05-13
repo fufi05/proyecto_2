ae0e33e4abb81fc4ea0f159acd59182d05dafab4
Cipher900 <gmorgan775@gmail.com>
2025-05-12 22:14:56 -0600

    Cambio primer mux

--- src/design/module_mux_4to1.sv
+++ src/design/module_mux_4to1.sv
@@ -6,12 +6,14 @@ module mux_4to1 (
     input [1:0] sel,               
     output reg [6:0] out
 );
-    always @(a or b or c or d or sel) begin
+    always_comb @(sel) begin
+        out = '0;
         case(sel)
             2'b00 : out <= a;
             2'b01 : out <= b;
             2'b10 : out <= c;
             2'b11 : out <= d;
         endcase
+        default: out = '0;
     end
 endmodule
